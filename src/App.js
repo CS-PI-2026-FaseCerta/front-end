2be65e2451bde14cc7a885f0c9f379ae95eda5b7
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-04-02 21:01:19 -0300

    feat-tela-sem-css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CadastroServico from "./auth/pages/CadastroServico";
+
+function App() {
+  return (
+    <div className="App">
+      <CadastroServico />
+    </div>
+  );
+}
+
+export default App;
