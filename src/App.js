b3c07916a7b86b75e31745c7a28d4122f7660172
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-04-07 21:59:00 -0300

    feat-clonando-cadastro-de-servico-para-usar-de-base

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Login from "./auth/pages/login";
-import CadastroServico from "./auth/pages/cadastroservico";
+import CadastroCidade from "./auth/pages/CadastroCidade";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         </Routes>
       </BrowserRouter> */}
 
-      <CadastroServico />
+      <CadastroCidade />
     </div>
   );
 }
