5961ebde8eb59efaa121fcf4f35f9c4992a10df3
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-05-22 22:00:13 -0300

    feat: implementando lista

--- src/form/pages/ServiceInsert/ServiceInsert.jsx
+++ src/form/pages/ServiceInsert/ServiceInsert.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Select from "react-select";
 import "./ServiceInsert.css";
 import { FaArrowLeft } from "react-icons/fa";
+import { HiOutlinePencil } from "react-icons/hi";
+import { FaRegTrashCan } from "react-icons/fa6";
 import Header from "../../../global/components/header/Header.jsx";
 import Footer from "../../../global/components/Footer/Footer.jsx"
 import { useNavigate } from "react-router-dom";
@@ -14,6 +16,15 @@ export default function ServiceInsert() {
     e.preventDefault();
 
   };
+
+  const servicos = [
+    {
+      descricao: "Instalar Lâmpada",
+      qntd: 1,
+      preco: 350,
+      obs: "Altura de 20 metros."
+    }
+  ]
 
   return (
     <div className="service-insert-page">
@@ -36,6 +47,23 @@ export default function ServiceInsert() {
               <button className="add-text-button">+ Adicionar Serviço</button>
             </div>
 
+            <div className="list">
+              {servicos.map((servico) => (
+                <Card
+                  descricao={servico.descricao}
+                  qntd={servico.qntd}
+                  preco={servico.preco}
+                  obs={servico.obs}
+                />
+              ))}
+            </div>
+
+            <div className="create-service-line">
+              <button className="add-text-button create">+ Criar Serviço</button>
+            </div>
+
+            
+
             <button
               type="submit"
               className="service-insert-form-submit-button"
@@ -49,4 +77,25 @@ export default function ServiceInsert() {
       <Footer />
     </div>
   );
+}
+
+function Card({descricao, qntd, preco, obs}) {
+  return (
+    <div className="card">
+      <div className="text">
+        <h3>{descricao}</h3>
+        <div className="description">
+          <p>{qntd}x</p>
+          <span>-</span>
+          <p>R$ {preco}</p>
+          <span>-</span>
+          <p>OBS: {obs}</p>
+        </div>
+      </div>
+      <div className="buttons">
+        <button><HiOutlinePencil size={20} color="var(--color-primary)" className="pencil-icon" /></button>
+        <button><FaRegTrashCan size={20} color="red" className="trash-icon" /></button>
+      </div>
+    </div>
+  )
 }
