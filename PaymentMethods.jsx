c2412b1ceed1316ee4ae1a5d2b7cbc08c60f0d47
victor-p3 <victorpldeoliveira@gmail.com>
2026-06-26 20:21:04 -0300

    criação das constantes das formas de pagamento e import dos padrões de css

--- PaymentMethods.jsx
+++ PaymentMethods.jsx
@@ -1,3 +1,17 @@
 import React, {useState} from "react";
 import {FaArrowLeft} from "react-icons/fa";
 import {useNavigate} from "react-router-dom"
+import "./PaymentMethods.css";
+import "../../../global/components/form/Form.css"
+import Header from "../../../global/components/header/Header.jsx"
+import Footer from "../../../global/components/Footer/Footer.jsx"
+
+const PAYMENT_METHODS = [
+    {id: "boleto", label: "Boleto"},
+    {id: "bank_transfer", label: "Transferência bancária"},
+    {id: "cash", label: "Dinheiro"},
+    {id: "check", label: "Cheque"}, 
+    {id: "credit_card", label: "Cartão de crédito"},
+    {id: "debit_card", label: "Cartão de débito"},
+    {id: "pix", label: "Pix"},
+]
