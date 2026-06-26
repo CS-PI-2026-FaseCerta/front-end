7e07f4042b40cb3fc153352ab99afafb10ed91f2
victor-p3 <victorpldeoliveira@gmail.com>
2026-06-26 20:39:29 -0300

    criação da função que seleciona os métodos de pagamento e comentários adicionados pra melhor entendimento do código

--- PaymentMethods.jsx
+++ PaymentMethods.jsx
@@ -1,3 +1,4 @@
+//imports necessários para o código
 import React, {useState} from "react";
 import {FaArrowLeft} from "react-icons/fa";
 import {useNavigate} from "react-router-dom"
@@ -6,6 +7,7 @@ import "../../../global/components/form/Form.css"
 import Header from "../../../global/components/header/Header.jsx"
 import Footer from "../../../global/components/Footer/Footer.jsx"
 
+//meios de pagamentos que aparecem na imagem de referencia
 const PAYMENT_METHODS = [
     {id: "boleto", label: "Boleto"},
     {id: "bank_transfer", label: "Transferência bancária"},
@@ -13,5 +15,18 @@ const PAYMENT_METHODS = [
     {id: "check", label: "Cheque"}, 
     {id: "credit_card", label: "Cartão de crédito"},
     {id: "debit_card", label: "Cartão de débito"},
-    {id: "pix", label: "Pix"},
-]
+    {id: "pix", label: "PIX"},
+];
+
+export default function PaymentMethods=(){
+    const navigate = useNavigate();
+
+    const[selectedMethods, setSelectedMethods] = useState([]);
+    const[successMessage, setSuccessMessage] = useState("");
+    //salva e exibe uma imagem de sucesso após o método ser selecionado para que o cliente tenha uma confirmação que foi salvo
+
+    const toggleMethod = (id) => {
+        setSelectedMethods(prev) => prev.includes(id)? prev.filter((item) => item !== id) : [...prev, id]
+    };
+    //função que checa se o método já está marcado como selecionado, se estiver, quando o cliente clicar novamente ele irá remove-lo do array dos métodos marcados
+};
