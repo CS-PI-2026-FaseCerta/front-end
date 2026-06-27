95d17e47022e2cc511e2053266325e7f68c6cefc
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-06-27 01:08:04 -0300

    refactor: servicosestoque em approutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -12,13 +12,13 @@ export const ApplyDiscounts = "/aplicarDesconto";
 export const Loading = "/loading";
 export const Pedidos = "/pedidos";
 export const Clientes = "/clientes";
-export const ServicosEstoque = "/servicos-estoque";
+export const Servicos = "/servicos";
+export const Estoque = "/estoque";
 
 //Não implementados ainda, mas já mapeados para facilitar futuras integrações.
 export const Perfil = "/perfil";
 export const Relatorios = "/relatorios";
 export const Configuracoes = "/configuracoes";
-export const ServicosEstoqueNovoItem = "/servicos-estoque/novo-item";
 export const OrdensServico = "/ordens-servico";
 export const OrdensServicoNovo = "/os/novo";
 export const Financeiro = "/financeiro";
