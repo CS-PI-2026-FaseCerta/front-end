eca79872fadf0b17041beabbf3ed00d0a6f752e2
danielalvesmoura <danielalvesmoura2006@gmail.com>
2026-08-07 22:17:10 -0300

    feat: adicionado na home financeiro

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ import CustomersListPage from "./home/pages/customers/CustomersListPage.jsx";
 import RegisterCustomer from "./form/pages/registercustomer/RegisterCustomer.jsx";
 import ProductsListPage from "./home/pages/products/ProductsListPage";
 
-import ListagemDespesas from "./finance/pages/expenselist/ExpenseList.jsx";
+import ExpenseList from "./finance/pages/expenselist/ExpenseList.jsx";
 
 import ServicesListPage from "./home/pages/services/ServicesListPage.jsx";
 import RegisterService from "./form/pages/RegisterService/RegisterService.jsx";
@@ -74,7 +74,6 @@ function App() {
             <Route path={AppRoutes.Servicos} element={<ServicesListPage />} />
             <Route path={AppRoutes.Pedidos} element={<OrdersListPage />} />
 
-            <Route path={AppRoutes.ListagemDespesas} element={<ListagemDespesas />} />
 
             <Route
               path={AppRoutes.ApplyDiscounts}
@@ -142,15 +141,7 @@ function App() {
             <Route
               path={AppRoutes.Financeiro}
               element={
-                <ProtectedRoute allowedProfiles={["gestor"]} redirectTo="/">
-                  <SectionPage
-                    eyebrow="Financeiro"
-                    title="Financeiro"
-                    description="Área restrita para gestores."
-                    ctaLabel="Voltar ao painel"
-                    ctaPath="/dashboard"
-                  />
-                </ProtectedRoute>
+                <ExpenseList/>
               }
             />
 
